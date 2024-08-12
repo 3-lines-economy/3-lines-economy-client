a0881de106119f9bc8062aacbfd1c1f615d6aa36
ChoiSangwon <fdsa200@naver.com>
2024-08-12 21:34:08 +0900

    chore : project init

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { createRequire } from "module";
+
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/main",
+        permanent: true,
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
