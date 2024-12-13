a295997b8e629eca1372bd0a1d0d16a626e6e776
ChoiSangwon <fdsa200@naver.com>
2024-12-14 01:33:47 +0900

    feat : kakao sdk 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import Header from "@/components/Header/Header";
 import type { AppProps } from "next/app";
 import RecoilRootWrapper from "./RecoilWrapper";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRootWrapper>
       <Header />
+      <Script
+        strategy="beforeInteractive"
+        src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=mhvjuj53xv"
+      />
+      {/* 카카오 로그인 위해 추가 */}
+      <script src="https://developers.kakao.com/sdk/js/kakao.js" defer></script>
       <Component {...pageProps} />
     </RecoilRootWrapper>
   );
