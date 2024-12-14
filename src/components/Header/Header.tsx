ae6cbbf4a79d59cef0adbbe52a0791713daa4acc
ChoiSangwon <fdsa200@naver.com>
2024-12-14 15:12:41 +0900

    feat : 저장버튼 추가

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,6 @@ import { authState } from "@/atoms/authState";
 import { useRouter } from "next/router";
 import KaKaoLoginSvg from "@public/kakao-login.svg";
 import SearchBar from "@/components/SearchBar/SearchBar";
-import KakaoLogin from "../KakaoLogin/KakaoLogin";
 
 const Header: React.FC = () => {
   const router = useRouter();
