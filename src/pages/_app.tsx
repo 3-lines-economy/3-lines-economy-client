e600c75fb1a93b07d9be16aa5763d4d5234eaa2e
ChoiSangwon <fdsa200@naver.com>
2024-12-19 22:01:16 +0900

    feat : 페이지 로딩 시 토큰 확인 함수 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,29 @@ import Header from "@/components/Header/Header";
 import type { AppProps } from "next/app";
 import RecoilRootWrapper from "./RecoilWrapper";
 import Script from "next/script";
+import { useSetRecoilState } from "recoil";
+import { authState } from "@/atoms/authState";
+import { useEffect } from "react";
+
+function InitializeAuthState() {
+  const setAuth = useSetRecoilState(authState);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem("accessToken");
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    if (accessToken && refreshToken) {
+      setAuth({ accessToken, refreshToken });
+    }
+  }, [setAuth]);
+
+  return null;
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <RecoilRootWrapper>
+      <InitializeAuthState />
       <Header />
       <Script
         strategy="beforeInteractive"
