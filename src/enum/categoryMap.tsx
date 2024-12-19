7a01204fd0c7809af3acd42e4b4698d38a4ad6be
ChoiSangwon <fdsa200@naver.com>
2024-12-19 22:00:15 +0900

    feat : 로그인 시 dropdown 추가

---
+++ src/enum/categoryMap.tsx
@@ -0,0 +1,9 @@
+export const categoryMap: { [key: string]: string } = {
+  ECONOMY: "경제",
+  POLITICS: "정치",
+  SOCIETY: "사회",
+  LIFESTYLE_CULTURE: "생활/문화",
+  IT_SCIENCE: "IT/과학",
+  WORLD: "세계",
+  ETC: "기타",
+};
