092126a0f2c973436dc77cfbd5f77361c42cf503
halfmoon-mind <5338095@gmail.com>
2025-03-19 01:33:35 +0900

    fix: 홈화면 기사 카테고리 텍스트 업데이트

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -11,7 +11,7 @@ import SideBar from "@/components/Sidebar/Sidebar";
 import { selectedArticleState } from "@/atoms/selectedArticleAtom";
 import CustomCalendar from "@/components/CustomCalendar/CustomCalendar";
 import { Post } from "@/types/post";
-import { CategoryMap, CategoryType } from "../../types/category";
+import { CategoryType, ScrapCategoryMap } from "../../types/category";
 
 const Main: React.FC = () => {
   const [currentPage, setCurrentPage] = useState(1);
@@ -120,7 +120,7 @@ const Main: React.FC = () => {
               <S.PostList>
                 {posts.map((post, index) => (
                   <S.PostItem key={post.id || index} onClick={() => handleArticleClick(post)}>
-                    <S.PostItemLeft>{CategoryMap[post.category as CategoryType] || post.category}</S.PostItemLeft>
+                    <S.PostItemLeft>{ScrapCategoryMap[post.category as CategoryType] || post.category}</S.PostItemLeft>
                     <S.PostItemCenter>{post.title}</S.PostItemCenter>
                     <S.PostItemRight>{post.publishedAt.split(" ")[0].replace(/-/g, ".")}</S.PostItemRight>
                   </S.PostItem>
